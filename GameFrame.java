8b71d4ba3931fd9b3692bb9931d7e5a42c409cee
VakZok <tobias.krebs1999@gmx.de>
2022-10-09 14:53:37 +0200

    Finished GameFrame class

--- GameFrame.java
+++ GameFrame.java
@@ -1,5 +1,13 @@
 import javax.swing.*;
 
 public class GameFrame extends JFrame {
-
+    public GameFrame(){
+        this.add(new GamePanel());
+        this.setTitle("Snake");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false); //game window will be of a fixed size
+        this.pack(); //fits JFrame around all of the components we add
+        this.setVisible(true);
+        this.setLocationRelativeTo(null); //make the game/GUI appear in the middle of the screen
+    }
 }
--- PushAndPlay.java
+++ PushAndPlay.java
@@ -1,5 +1,5 @@
 public class PushAndPlay {
     public static void main(String[] args) {
-        System.out.println("Hello there!");
+        new GameFrame();
     }
 }
